2d3bb903cf7b055ad995cefd6be9734352a8aab1
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-06-09 21:31:18 +0530

    Update Reverse Linked List.cpp

--- Reverse Linked List.cpp
+++ Reverse Linked List.cpp
@@ -1,15 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/*Alternate solution:
+ListNode* reverseList(ListNode* head) 
+{
+	ListNode* prev = NULL;
+	ListNode* follow = NULL;
+	while (head)
+	{
+		follow = head->next;
+		head->next = prev;
+		prev = head;
+		head = follow;
+	}
+    return prev;
+}
+bool hasCycle(ListNode *head)
+{
+	ListNode* rev = reverseList(head);
+	if (head && head->next && rev == head)
+	{
+		return true;
+	}
+	return false;
+}
+if the list has got a loop, then its reversed version must have got the same head pointer as its self;
+*/
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* current = head, *prev = NULL, *next = NULL;
-        while(current)
+    bool hasCycle(ListNode *head) {
+        if(!head)
+            return false;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(slow && fast)
         {
-            next = current->next;
-            current->next = prev;
-            prev = current;
-            current = next;
+            if(slow == fast)
+                break;
+            if(fast->next)
+            {
+                 slow = slow->next;
+                 fast = fast->next->next;
+            }
+            else 
+                break;
         }
-        head = prev;
-        return head;
+        if(slow == fast && slow)
+            return true;
+        else
+            return false;
     }
 };
