1644a815878c9d06a21cf68c98947d8fd5597ccb
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-06-20 08:49:50 +0530

    Create Generate Parentheses.cpp

---
+++ Generate Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void helper(int open,int close,int n,string current, vector<string> &result)
+    {
+        if(current.length()==n*2)
+        {
+            result.push_back(current);
+            return ;
+        }
+        if(open<n)  helper(open+1,close,n,current+"(",result);
+        if(close<open)  helper(open,close+1,n,current+")",result);
+    }
+    vector<string> generateParenthesis(int n) {
+         vector<string>result;
+        helper(0,0,n,"",result);
+        return result;
+    }
+};
