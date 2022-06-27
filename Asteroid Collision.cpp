7d03340dc369bacccf4542d064b9ca919d19ba38
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-06-27 21:25:15 +0530

    Create Asteroid Collision.cpp

---
+++ Asteroid Collision.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> s;
+        for(int i = 0; i < n; i++) {
+            if(ast[i] > 0 || s.empty()) {
+                s.push(ast[i]);
+            }
+            else {
+                while(!s.empty() and s.top() > 0 and s.top() < abs(ast[i])) {
+                    s.pop();
+                }
+                if(!s.empty() and s.top() == abs(ast[i])) {
+                    s.pop();
+                }
+                else {
+                    if(s.empty() || s.top() < 0)//and if s.top() is > 0, then it already has magnitude > abs(ast[i]) because of the above while loop, thus ast[i] won't be pushed into the stack in this case due to its collision with a bigger magnitude positive element and we know that ast[i] is < 0 .
+                    {
+                        s.push(ast[i]);
+                    }
+                }
+            }
+        }
+		// finally we are returning the elements which remains in the stack.
+		// we have to return them in reverse order.
+        vector<int> res(s.size());
+        for(int i = (int)s.size() - 1; i >= 0; i--) {
+            res[i] = s.top();
+            s.pop();
+        }
+        return res;
+    }
+};
