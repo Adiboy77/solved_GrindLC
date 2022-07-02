f092906f5d5591164870f27208142465782646ce
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-07-02 18:19:48 +0530

    Update Next Permutation.cpp

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -1,3 +1,16 @@
+/*
+Why do we reverse here:
+1,2,4,5,3,4,3,2,1
+iski next perm: 1,2,4,5,3,1,2,3,4
+
+ye isliye because jo first while loop hai that will break at 4(index 5(0based)).
+So fir jo ye subsegment hai 4,3,2,1 last ka , ye hmne decreasing order me find kra hai through first while loop, so next permutation of any decreasing order = reverse of that order only(ex.3,2,1 ->nextperm= 1,2,3)
+
+isiliye reverse krrhe
+
+This is the working principle of the whole code basically :
+so next permutation of any decreasing order = reverse of that order only(ex.3,2,1 ->nextperm= 1,2,3)
+*/
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
