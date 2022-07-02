d3bc23adf662be810a23f60e7e83d6ba2d260f3c
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-07-02 16:00:56 +0530

    Create Clone Graph.cpp

---
+++ Clone Graph.cpp
@@ -0,0 +1,48 @@
+/*Using DFS:
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return NULL;
+        }
+        if (copies.find(node) == copies.end()) {
+            copies[node] = new Node(node -> val, {});
+            for (Node* neighbor : node -> neighbors) {
+                copies[node] -> neighbors.push_back(cloneGraph(neighbor));
+            }
+        }
+        return copies[node];
+    }
+private:
+    unordered_map<Node*, Node*> copies;
+};
+*/
+//Using BFS:
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return NULL;
+        }
+        Node* copy = new Node(node -> val, {});
+        copies[node] = copy;
+        queue<Node*> todo;
+        todo.push(node);
+        while (!todo.empty()) {
+            Node* cur = todo.front();
+            todo.pop();
+            for (Node* neighbor : cur -> neighbors) 
+            {
+                if (copies.find(neighbor) == copies.end()) 
+                {
+                    copies[neighbor] = new Node(neighbor -> val, {});
+                    todo.push(neighbor);
+                }
+                copies[cur] -> neighbors.push_back(copies[neighbor]);//This line is adding the neighbors to the copy node (as copy node is initialized with its value only - line 27), note that these neighbors are also copy of the neighbors of original node.
+            }
+        }
+        return copy;
+    }
+private:
+    unordered_map<Node*, Node*> copies;
+};
