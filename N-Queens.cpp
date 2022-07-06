b719801394d23193957cc606f1d997b8c0a06392
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-07-06 08:55:51 +0530

    Create N-Queens.cpp

---
+++ N-Queens.cpp
@@ -0,0 +1,38 @@
+//In this problem, we can go row by row, and in each position, we need to check if the column, the 45° diagonal and the 135° diagonal had a queen before.
+class Solution {
+public:
+        vector<vector<string> > solveNQueens(int n) {
+        vector<vector<string> > res;
+        vector<string> nQueens(n, string(n, '.'));
+        solveNQueens(res, nQueens, 0, n);
+        return res;
+    }
+private:
+    void solveNQueens(vector<vector<string> > &res, vector<string> &nQueens, int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (isValid(nQueens, row, col, n)) {
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, row + 1, n);
+                nQueens[row][col] = '.';//backtracking to the original value if solution is not found
+            }
+    }
+    bool isValid(vector<string> &nQueens, int row, int col, int &n) {
+        //check if the column had a queen before.
+        for (int i = 0; i != row; ++i)
+            if (nQueens[i][col] == 'Q')
+                return false;
+        //check if the 45° diagonal had a queen before.
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        //check if the 135° diagonal had a queen before.
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        return true;
+    }
+};
