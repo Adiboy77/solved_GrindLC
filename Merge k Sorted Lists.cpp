1756cdf588de48b2c71b8a74c2a0542e7508dd09
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-07-25 13:48:03 +0530

    Create Merge k Sorted Lists.cpp

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,33 @@
+//Time: O(nlogk)
+//Space: 
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // Create an ordered map
+        map<int, int> nodes;
+        
+        // Add all nodes of all the LinkedLists to the map with
+        // with the ListNode->val as the key & that value's frequency as the mapped value
+        for(int i = 0; i < lists.size(); ++i){
+            auto node = lists[i];
+            while(node){
+                int temp = node->val;
+                nodes[temp]++;
+                node = node->next;
+            }
+        }
+        
+        ListNode dummyHead(0);
+        ListNode* tail = &dummyHead;
+        
+        for(auto it: nodes){
+            while(it.second != 0){
+                ListNode* newNode= new ListNode(it.first);
+                tail->next = newNode;
+                tail = tail->next;
+                it.second--;
+            }
+        }
+        return dummyHead.next;
+    }
+};
