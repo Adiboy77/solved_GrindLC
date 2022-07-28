9e1ca45dd36efd56860941b2efe846ea577a43ed
Aditya Vikram Tiwari <adityavikramtiwari7@gmail.com>
2022-07-28 14:18:15 +0530

    Update Valid Anagram.cpp

--- Valid Anagram.cpp
+++ Valid Anagram.cpp
@@ -1,3 +1,34 @@
+/*
+Follow up:
+For the follow up part where unicode characters are used, we can just increase the size of array to number of characters in unicode and first letter's ASCII code could be adjusted to zero.
+
+Once the map(array) is ready, we can now iterate over the second string and decrease the frequencies of corresponding elements on each iteration.
+Then finally, iterate over the map and check if any element is non-zero and return false in this case, otherwise return true.
+Like:
+//help in easy switch to follow up case
+#define CHAR_COUNT 26
+#define FIRST_CHAR 97   //ASCII code of first char('a' in this case)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+
+        int freqArray[CHAR_COUNT] = {0};
+		
+        for(int i = 0; i < s.length(); i++){
+            ++freqArray[s[i] - FIRST_CHAR];
+            --freqArray[t[i] - FIRST_CHAR];
+        }
+
+        for(int i = 0; i < CHAR_COUNT; i++)
+            if(freqArray[i]!=0) return false;
+        return true;
+    }
+};
+
+//Or if unicode characters are say > 10^6, then we can simply sort the given strings and compare and return because we cannot create a space for that much of a large array
+*/
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -9,10 +40,6 @@ public:
              freq[s[i]-'a']++;
             cmp[t[i]-'a']++;
         }
-           
-        for(int i=0;i<26;i++)
-            if(freq[i]!=cmp[i])
-                return false;
-        return true;
+        return freq == cmp ;
     }
 };
